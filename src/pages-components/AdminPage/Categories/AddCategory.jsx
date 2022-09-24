c6014c730cc47382b268f817df331ae8a3fc7a24
kdubasov <cergocergo41@gmail.com>
2022-09-24 23:56:31 +0300

    redact params handleSend and add comments

--- src/pages-components/AdminPage/Categories/AddCategory.jsx
+++ src/pages-components/AdminPage/Categories/AddCategory.jsx
@@ -1,41 +1,48 @@
 import React, {useState} from 'react';
-import {Button} from "react-bootstrap";
+import {Button, FloatingLabel, Form} from "react-bootstrap";
 import {handleAddCategory} from "../../../pages-functions/AdminPage/Categories/handleAddCategory";
 
 const AddCategory = () => {
 
+    //for error or result form
     const [err,setErr] = useState(false)
     const [res,setRes] = useState(false)
 
+    //inps value
     const [category,setCategory] = useState({
         id:'',
         title:'',
         image:'',
     })
 
+    //for changes inputs
     const handleChange = (value,id) =>{
         const copy  = Object.assign({}, category);
         copy[id] = value;
         setCategory(copy);
     }
 
+    //get input for verstka
     const getInput = value =>{
         return(
-            <input
-                placeholder={value}
-                value={category[value]}
-                onChange={e => handleChange(e.target.value,value)}
-            />
+            <FloatingLabel label={value} className={`mt-1 mb-1`}>
+                <Form.Control
+                    value={category[value]}
+                    onChange={e => handleChange(e.target.value,value)}
+                    placeholder={value}
+                />
+            </FloatingLabel>
         )
     }
 
-    const handleSendForm = async (e) =>{
+    //send form func with check errors or res
+    const handleSendForm = async (e,url) =>{
         e.preventDefault()
-        await handleAddCategory(e,"/categories",category.id,category.title,category.image)
+        await handleAddCategory(e,url,category.id,category.title,category.image)
             .then(() => setRes(true))
             .catch(() => setErr(true))
 
-        if (res){
+        if (res){//check res or err
             setTimeout(() => setRes(false),5000)
         }else {
             setTimeout(() => setRes(false),5000)
@@ -44,8 +51,10 @@ const AddCategory = () => {
     }
 
     return (
-        <div className={`AddCategory`}>
-            <form onSubmit={e => handleSendForm(e)}>
+        <div className={`AddCategory w-100 d-flex`}>
+            <Form className={`w-50 border p-3`} onSubmit={e => handleSendForm(e,"/categories")}>
+
+                <h5>Добавить категорию</h5>
 
                 {
                     Object.keys(category).map(elem =>(
@@ -61,7 +70,7 @@ const AddCategory = () => {
                 </span>
 
                 <Button type={"submit"}>Add</Button>
-            </form>
+            </Form>
         </div>
     );
 };
